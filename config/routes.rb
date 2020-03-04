83c3c7cf0b560edac6f69b957ac3a931a55eda94
IvanMonroy <inter250296@hotmail.com>
2020-03-03 22:05:33 -0500

    index for menu service

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     resources :products, only: %i[index create update show destroy] do
 
     end
-    resources :index_for_menu, only: %i[index create update show destroy] do
+    resources :works, only: %i[index create update show destroy] do
       collection do
         get 'index_gallery/:id', action: :index_for_gallery
         get 'index_menu/', action: :index_for_menu
