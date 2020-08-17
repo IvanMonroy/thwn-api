6f3a4c1c116d7c6f4b6ec338aae30719b28d0db0
IvanMonroy <inter250296@hotmail.com>
2020-08-17 00:23:05 -0500

    CHANGE TOS POST SERVICE

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
-class ApplicationController < ActionController::Base
-  protect_from_forgery unless: -> { request.format.json? }
+class ApplicationController < ActionController::API
+  protect_from_forgery with: :null_session
   def authenticate_user!
     return true
   end
