f75e7da6470a76792f07eb6e1b8bc0d086dac310
IvanMonroy <inter250296@hotmail.com>
2020-08-17 17:49:33 -0500

    Intentando solucionar problema de los cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :null_session
-  before_filter :cor
-  def cor
-    headers["Access-Control-Allow-Origin"]  = "*"
-    headers["Access-Control-Allow-Methods"] = %w{GET POST PUT DELETE}.join(",")
-    headers["Access-Control-Allow-Headers"] = %w{Origin Accept Content-Type X-Requested-With X-CSRF-Token}.join(",")
-    head(:ok) if request.request_method == "OPTIONS"
-  end
+  # protect_from_forgery with: :null_session
 
   def authenticate_user!
     return true
